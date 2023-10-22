91e1fb07096ac648b76b7ea2c5513a8b087a6e60
Tanzim <tanzim008@gmail.com>
2023-10-22 17:59:44 -0230

    components created

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+      <img src='' alt='logo'/>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Works</a></li>
+        <li><a href='#'>Contact</a></li>
+      </ul>
+    </>
+  )
+}
+
+export default Nav
